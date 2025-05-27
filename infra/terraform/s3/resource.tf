fc667a9c677f20854aa6802293f93c33d152ac33
HuyAtreides <huyatreides@gmail.com>
2025-05-27 21:40:54 +0700

    add storage for jelly fin

--- infra/terraform/s3/resource.tf
+++ infra/terraform/s3/resource.tf
@@ -7,44 +7,38 @@ resource "aws_s3_bucket" "mopic_config" {
 }
 
 resource "aws_s3_object" "mopic_data_radarr" {
-  bucket       = aws_s3_bucket.mopic_data.id
-  key          = "/mopic/data/radarr"
-  content      = "" # Empty object
-  content_type = "text/plain"
+  bucket  = aws_s3_bucket.mopic_data.id
+  key     = "/mopic/data/radarr"
+  content = ""
 }
 
 resource "aws_s3_object" "mopic_data_qbittorrent" {
-  bucket       = aws_s3_bucket.mopic_data.id
-  key          = "/mopic/data/qbittorrent/"
-  content      = "" # Empty object
-  content_type = "text/plain"
+  bucket  = aws_s3_bucket.mopic_data.id
+  key     = "/mopic/data/qbittorrent/"
+  content = ""
 }
 
 resource "aws_s3_object" "mopic_data_media" {
-  bucket       = aws_s3_bucket.mopic_data.id
-  key          = "/mopic/data/media/"
-  content      = "" # Empty object
-  content_type = "text/plain"
+  bucket  = aws_s3_bucket.mopic_data.id
+  key     = "/mopic/data/media/"
+  content = ""
 }
 
 
 resource "aws_s3_object" "mopic_config_radarr" {
-  bucket       = aws_s3_bucket.mopic_config.id
-  key          = "/mopic/config/radarr"
-  content      = "" # Empty object
-  content_type = "text/plain"
+  bucket  = aws_s3_bucket.mopic_config.id
+  key     = "/mopic/config/radarr"
+  content = ""
 }
 
 resource "aws_s3_object" "mopic_config_prowlarr" {
-  bucket       = aws_s3_bucket.mopic_config.id
-  key          = "/mopic/config/prowlarr"
-  content      = "" # Empty object
-  content_type = "text/plain"
+  bucket  = aws_s3_bucket.mopic_config.id
+  key     = "/mopic/config/prowlarr"
+  content = ""
 }
 
 resource "aws_s3_object" "mopic_config_qbittorent" {
-  bucket       = aws_s3_bucket.mopic_config.id
-  key          = "/mopic/config/qbittorrent"
-  content      = "" # Empty object
-  content_type = "text/plain"
+  bucket  = aws_s3_bucket.mopic_config.id
+  key     = "/mopic/config/qbittorrent"
+  content = ""
 }
