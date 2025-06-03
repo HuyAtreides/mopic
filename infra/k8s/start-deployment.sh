6972a2a09dc0272107a14522a1fe0d9c893fe9d8
HuyAtreides <huyatreides@gmail.com>
2025-06-03 15:46:27 +0700

    update persistent volume to use eks auto class name

--- infra/k8s/start-deployment.sh
+++ infra/k8s/start-deployment.sh
@@ -1,1 +1,1 @@
-kubectl apply -f mopic_storage_class.yaml && kubectl apply -f mopic_volume_claim.yaml && kubectl apply -f mopic_deployment.yaml
+kubectl apply -f mopic_storage_class.yaml && kubectl apply -f mopic_persistent_volume.yaml && kubectl apply -f mopic_volume_claim.yaml && kubectl apply -f mopic_deployment.yaml
