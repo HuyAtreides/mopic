b1d02bbd72a4145fc230a2c3b833bb72c6d17009
HuyAtreides <huyatreides@gmail.com>
2025-06-06 18:10:18 +0700

    udpate config to deploy on gpu-ec2

--- infra/k8s/start-deployment.sh
+++ infra/k8s/start-deployment.sh
@@ -1,3 +1,4 @@
+kubectl apply -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v0.14.1/nvidia-device-plugin.yml
 kubectl apply -f mopic_storage_class.yaml 
 kubectl apply -f mopic_service.yaml
 # kubectl apply -f mopic_persistent_volume.yaml 
--- infra/terraform/eks_cluster/data.tf
+++ infra/terraform/eks_cluster/data.tf
@@ -20,3 +20,14 @@ data "aws_eks_cluster_auth" "mopic_k8s" {
   name = data.aws_eks_cluster.mopic_k8s.name
 }
 
+# data "aws_ssm_parameter" "eks_gpu_ami" {
+#   name = "/aws/service/eks/optimized-ami/1.32/amazon-linux-2-gpu/recommended/image_id"
+# }
+
+# data "aws_ami" "eks_gpu" {
+#   owners = ["602401143452"] # Amazon EKS
+#   filter {
+#     name   = "image-id"
+#     values = [data.aws_ssm_parameter.eks_gpu_ami.value]
+#   }
+# }
