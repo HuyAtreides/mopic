771706a61ff2d9a74807e528f15ed830723b4286
HuyAtreides <huyatreides@gmail.com>
2025-06-07 15:19:58 +0700

    upgrade ebs type

--- infra/k8s/start-deployment.sh
+++ infra/k8s/start-deployment.sh
@@ -1,4 +1,5 @@
-kubectl apply -f mopic_storage_class.yaml 
+kubectl apply -f mopic_media_storage_class.yaml 
+kubectl apply -f mopic_config_storage_class.yaml 
 kubectl apply -f mopic_service.yaml
 # kubectl apply -f mopic_persistent_volume.yaml 
 kubectl apply -f mopic_volume_claim.yaml 
