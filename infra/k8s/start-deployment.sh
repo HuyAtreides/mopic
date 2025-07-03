4fd74567afefd60164dee672ca67fbd03fa2f7d0
HuyAtreides <huyatreides@gmail.com>
2025-07-03 18:56:18 +0700

    implement restore volume from snapshot

--- infra/k8s/start-deployment.sh
+++ infra/k8s/start-deployment.sh
@@ -5,6 +5,10 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snaps
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml
 
+kubectl apply -f mopic_volume_snapshot_class.yaml
+kubectl apply -f mopic_volume_snapshot_content.yaml
+kubectl apply -f mopic_volume_snapshot.yaml
+
 kubectl apply -f mopic_media_storage_class.yaml 
 kubectl apply -f mopic_config_storage_class.yaml 
 kubectl apply -f mopic_service.yaml
